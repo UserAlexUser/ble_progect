83604821c693ab6eacd328efd409dd8997c0f070
UserAlexUser <alexandrit96@yandex.ru>
2022-06-06 17:51:22 +0000

    delete_unused_function

--- include/color_converter.h
+++ include/color_converter.h
@@ -17,7 +17,5 @@ typedef struct
 }hsv_t;
 
 void rgb2hsv(const rgb_t *rgb, hsv_t *hsv);
-void rgb_to_hsv(const rgb_t *rgb, hsv_t* hsv);
 void hsv2rgb(const hsv_t *hsv, rgb_t *rgb);
-void hsv_to_rgb(const hsv_t *hsv, rgb_t *rgb);
 #endif
